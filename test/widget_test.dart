03acf5cbd904aeee25be17040b3d1c8b587783e2
SamirBenj <sciencecalc7@gmail.com>
2022-05-23 16:22:00 +0200

    test another

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_application_1/main.dart';
 
 void main() {
+  print('hello');
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
   //   // Build our app and trigger a frame.
   //   await tester.pumpWidget(MyApp());
